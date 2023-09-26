e91cfadcbe2e2ce5c453c49eb9e6f732d92e6d5d
kaynekaynekayne <theygoon@gmail.com>
2023-09-26 15:03:38 +0900

    m

--- pj/src/renderer/components/LoginPage.vue
+++ pj/src/renderer/components/LoginPage.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>ddwewe</div>
+    <b-container>
+        <img id="imgLogo" src="~@/assets/login/logo.png" alt="logo">
+        <div id="divInputForm">
+            
+        </div>
+    </b-container>
 </template>
 
 <script>
     export default {
-    
+        name:'login-page'
     }
 </script>
