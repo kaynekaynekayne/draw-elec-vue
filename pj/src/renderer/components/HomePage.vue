28f10ac57860b016a291a263fe37bf4a0a683443
kaynekaynekayne <theygoon@gmail.com>
2023-09-26 17:23:16 +0900

    homepage

--- pj/src/renderer/components/HomePage.vue
+++ pj/src/renderer/components/HomePage.vue
@@ -1,9 +1,36 @@
 <template>
-    <div>dhome</div>
+    <!-- b-overlay 전체를 감싼다-->
+    <b-overlay :show="overlayShow" opacity="0.5" rounded="sm" style="color:white">
+        <!--빙글빙글 도는 원형 화살표-->
+        <template v-slot:overlay>
+            <div class="text-center">
+                <b-icon icon="arrow-clockwise" font-scale="3" animation="spin"></b-icon>
+                <p id="cancel-label">please wait...</p>
+            </div>
+        </template>
+
+        <!--상단 메뉴-->
+        <div class="navHome">
+            <b-row>
+                <b-col>
+                    <b-row> 
+                        <b-col v-for="(p, i) in pages" :key="i" @click="onClickTab(p)"></b-col>
+                    </b-row>
+                </b-col>
+                <b-col></b-col>
+                <b-col></b-col>
+                <b-col></b-col>
+            </b-row>
+        </div>
+    </b-overlay>
 </template>
 
 <script>
     export default {
-    
+        methods:{
+            onClickTab(page){
+                
+            }
+        }
     }
 </script>
